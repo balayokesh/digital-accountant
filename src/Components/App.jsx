be21f09a2dce4ba1d330dd27601cf4dbbf3e7091
balayokesh <balayokeshmani@gmail.com>
2021-11-07 15:55:55 +0530

    added material ui

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,8 +1,13 @@
+import { 
+  CssBaseline,
+ } from '@material-ui/core';
+
 import '../Styles/App.css';
 
 function App() {
   return (
     <div>
+      <CssBaseline />
       <h1>Digital Accountant</h1>
     </div>
   );
